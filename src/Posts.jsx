e7a4773beb293b7647882351cdbe26a961796cc1
N00bG1rl <403helen@gmail.com>
2022-03-20 10:51:15 +0200

    Prefetch next page

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,23 +1,42 @@
-import { useState } from 'react'
-import { useQuery } from 'react-query'
+import { useEffect, useState } from 'react'
+import { useQuery, useQueryClient } from 'react-query'
 
 import { PostDetail } from './PostDetail'
 const maxPostPage = 10
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
 	const response = await fetch(
-		'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+		`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
 	)
 	return response.json()
 }
 
 export function Posts() {
-	const [currentPage, setCurrentPage] = useState(0)
+	const [currentPage, setCurrentPage] = useState(1)
 	const [selectedPost, setSelectedPost] = useState(null)
 
-	// replace with useQuery
-	const { data, isError, error, isLoading } = useQuery('posts', fetchPosts)
+	const queryClient = useQueryClient()
+
+	// Prefetch next page
+	useEffect(() => {
+		if (currentPage < maxPostPage) {
+			const nextPage = currentPage + 1
+			queryClient.prefetchQuery(['posts', nextPage], () => fetchPosts(nextPage))
+		}
+	}, [currentPage, queryClient])
+
+	const { data, isError, error, isLoading } = useQuery(
+		// Keep track of current page
+		['posts', currentPage],
+		() => fetchPosts(currentPage),
+		{
+			staleTime: 2000,
+			keepPreviousData: true,
+		}
+	)
+
 	if (isLoading) return <h3>Loading...</h3>
+
 	if (isError)
 		return (
 			<>
@@ -25,6 +44,7 @@ export function Posts() {
 				<p>{error.toString}</p>
 			</>
 		)
+
 	//if (!data) return <div />
 
 	return (
@@ -41,11 +61,21 @@ export function Posts() {
 				))}
 			</ul>
 			<div className='pages'>
-				<button disabled onClick={() => {}}>
+				<button
+					disabled={currentPage <= 1}
+					onClick={() => {
+						setCurrentPage(prevValue => prevValue - 1)
+					}}
+				>
 					Previous page
 				</button>
-				<span>Page {currentPage + 1}</span>
-				<button disabled onClick={() => {}}>
+				<span>Page {currentPage}</span>
+				<button
+					disabled={currentPage >= maxPostPage}
+					onClick={() => {
+						setCurrentPage(prevValue => prevValue + 1)
+					}}
+				>
 					Next page
 				</button>
 			</div>
